a6b9d43e4a11b065378ddb8ec3c8d5f1fefbcd1a
jacopo1996 <jacopo1996@live.it>
2022-05-10 17:58:38 +0200

    Update commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,19 +2,20 @@
 document.body.style.overflow = "hidden";
 
 //variabili oggetti documento
-let search = document.querySelector('[button-search]');
-let div_result = document.querySelector('[result]');
+const search = document.querySelector('[button-search]');
+const main = document.querySelector('[main]');
 
-let number;
-
-// richiesta dei valori al server
+//richiesta dei valori al server
 async function get(url, city) {
-  let response = await fetch(url);
+  const response = await fetch(url);
   if (response.ok) {
     let json = await response.json();
     print(json);
   } else {
-    div_result.setAttribute("style","visibility: hidden");
+    if ( main.lastElementChild.hasAttribute("result") ) {
+      div_result = document.querySelector('[result]');
+      div_result.remove();
+    }
     Swal.fire({ //messaggio avviso città non trovata
       title: `The city "${city}" was not found`,
       showCancelButton: false,
@@ -23,24 +24,38 @@ async function get(url, city) {
   }
 }
 
-// stampa a video dei valori
+//stampa a video dei valori
 async function print(text) {
-  div_result.innerHTML = "";
+  if ( main.lastElementChild.hasAttribute("result") ) {
+    div_result.innerHTML = "";
+  } else {
+    div_result = document.createElement('div');
+    div_result.className = "result";
+    div_result.setAttribute("result","");
+  }
   for (i = 0; i <= 18; i++) {
-    if (i <= 16) {
+    if ( i <= 16 ) {
       div_result.innerHTML += `<p style="color: ${text.categories[i].color}">${text.categories[i].name}: ${text.categories[i].score_out_of_10.toPrecision(3)}</p>`;
-    } else if (i == 17) {
+    } else if ( i == 17 ) {
       div_result.innerHTML += `<p>Teleport City Score: ${text.teleport_city_score.toPrecision(3)}</p>`
     } else {
       div_result.innerHTML += `${text.summary}`
     }
   }
-  div_result.removeAttribute("style");
+  main.append(div_result);
 }
 
-// click sul pulsante "search"
+//click sul pulsante "search"
 search.onclick = function() {
   let textbox = document.querySelector('[textbox-city]').value;
-  let city = textbox.replace(/\s+/g, '-').toLowerCase();
-  get(`https://api.teleport.org/api/urban_areas/slug:${city}/scores/`, textbox);
+  if ( textbox == "" ) {
+    Swal.fire({ //messaggio avviso città non trovata
+      title: "You have not entered any city name",
+      showCancelButton: false,
+      confirmButtonColor: "#ff0000"
+    });
+  } else {
+    let city = textbox.replace(/\s+/g, '-').toLowerCase();
+    get(`https://api.teleport.org/api/urban_areas/slug:${city}/scores/`, textbox);
+  }
 }
